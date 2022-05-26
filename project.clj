0c42ff2482fb3770c3ef6532fee537f4d898830d
David Pritchard <david.al.pritchard@gmail.com>
2022-05-25 22:35:04 -0400

    add better project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lisp-in-small-pieces "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure implementation of Queinnec's Lisp in Small Pieces"
+  :url "https://github.com/dapritchard/lisp-in-small-pieces-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
