becfdac334ba822f2c48077cb3e941e45ccdf310
David Pritchard <david.al.pritchard@gmail.com>
2022-05-28 11:36:57 -0400

    add extend-env tests

--- test/lisp_in_small_pieces/core_test.clj
+++ test/lisp_in_small_pieces/core_test.clj
@@ -12,7 +12,8 @@
 ;;   (is (= (lookup 'a ()))))
 
 (deftest test-extend-env
-  (testing "extend-env"
+
+  (testing "extend-env emtpy env"
     (is (= (extend-env () () ())
            ()))
     (is (= (extend-env () '(a) '(1))
@@ -20,4 +21,35 @@
     (is (= (extend-env () '(a bb) '(1 "two"))
            '((a 1)
              (bb "two"))))
-    ))
+    (is (= (extend-env () 'a 1)
+           '((a 1)))
+        "non-list `variable` and `value`"))
+
+  (testing "extend-env nonempty env"
+      (is (= (extend-env '((z 9)) () ())
+             '((z 9))))
+      (is (= (extend-env '((z 9)) '(a) '(1))
+             '((a 1)
+               (z 9))))
+      (is (= (extend-env '((z 9)) '(z) '(26))
+             '((z 26)
+               (z 9)))
+          "never symbol 'shadows' existing")
+      (is (= (extend-env '((z 9)) '(a bb) '(1 "two"))
+             '((a 1)
+               (bb "two")
+               (z 9))))
+      (is (= (extend-env '((z 9)) '(a bb z) '(1 "two" 26.0))
+             '((a 1)
+               (bb "two")
+               (z 26.0)
+               (z 9))))
+      (is (= (extend-env '((z 9) (q "four")) '(a bb) '(1 "two"))
+             '((a 1)
+               (bb "two")
+               (z 9)
+               (q "four"))))
+      (is (= (extend-env '((z 9)) 'a 1)
+             '((a 1)
+               (z 9)))
+          "non-list `variable` and `value`")))
